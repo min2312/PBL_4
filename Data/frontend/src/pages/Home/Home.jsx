b9182f1a7119d7f1dd43d8b95e852aa009d77ba2
Nguyen Quang Minh <quangminh23122004@gmail.com>
2024-10-16 09:00:43 +0700

    Add UI Home

--- Data/frontend/src/pages/Home/Home.jsx
+++ Data/frontend/src/pages/Home/Home.jsx
@@ -1,7 +1,51 @@
 import React from "react";
-
+import "../Home/banner.css";
+import { Carousel } from "react-bootstrap";
+import sliderImg from "../../assets/images/1.avif";
+import sliderImg1 from "../../assets/images/2.avif";
 const Home = () => {
-	return <div>Hello Home</div>;
+	return (
+		<section className="slider">
+			<Carousel variant="dark">
+				<Carousel.Item>
+					<img
+						src={sliderImg}
+						className="d-block w-100 slider_img"
+						alt="First slide"
+					/>
+					<Carousel.Caption>
+						<div className="slider_des">
+							<h5 className="heading">
+								SMART PARKING SYSTEM <span>FOR MODERN DRIVERS</span>
+							</h5>
+							<p className="sub_text">
+								Effortlessly manage your parking needs with real-time updates
+								and seamless booking options.
+							</p>
+						</div>
+					</Carousel.Caption>
+				</Carousel.Item>
+				<Carousel.Item>
+					<img
+						src={sliderImg1}
+						className="d-block w-100 slider_img"
+						alt="Second slide"
+					/>
+					<Carousel.Caption>
+						<div className="slider_des">
+							<h5 className="heading">
+								YOUR ULTIMATE SOLUTION <span>TO HASSLE-FREE PARKING</span>
+							</h5>
+							<p className="sub_text">
+								Experience secure and convenient parking with automated services
+								and real-time availability.
+							</p>
+						</div>
+					</Carousel.Caption>
+				</Carousel.Item>
+			</Carousel>
+		</section>
+	);
 };
 
 export default Home;
