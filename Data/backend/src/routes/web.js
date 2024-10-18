9021baba1d4e4a9d097f06cdbd1fd197ddf83aea
Nguyen Quang Minh <quangminh23122004@gmail.com>
2024-10-18 08:18:34 +0700

    Add api Ticket

--- Data/backend/src/routes/web.js
+++ Data/backend/src/routes/web.js
@@ -2,6 +2,7 @@ import express from "express";
 import userController from "../controllers/userController";
 import adminController from "../controllers/adminController";
 import { checkUserJWT } from "../middleware/JWT_Action";
+import apiController from "../controllers/apiController";
 let router = express.Router();
 
 let initWebRoutes = (app) => {
@@ -10,9 +11,14 @@ let initWebRoutes = (app) => {
 	router.post("/api/admin_login", adminController.HandleLoginAdmin);
 	router.post("/api/logout", userController.HandleLogOut);
 	router.get("/api/get-all-user", userController.HandleGetAllUser);
+	router.get("/api/get-info-car", userController.HandleGetInfoCar);
+	router.get("/api/getAllCar", apiController.HandleGetAllCar);
 	router.get("/api/account", userController.getUserAccount);
+	router.get("/api/getTypeTicket", apiController.HandleGetTypeTicket);
 	router.put("/api/edit-user", userController.HandleEditUser);
 	router.post("/api/create-new-user", userController.HandleCreateNewUser);
+	router.post("/api/create-new-car", apiController.HandleCreateNewCar);
+	router.post("/api/create-ticket", apiController.HandleCreatePayment);
 	router.delete("/api/delete-user", userController.HandleDeleteUser);
 	return app.use("/", router);
 };
