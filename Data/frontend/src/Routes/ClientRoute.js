cee3f918ccd0ce7999ffdd9948f6072eb652b4b3
Nguyen Quang Minh <quangminh23122004@gmail.com>
2024-10-18 08:24:23 +0700

    Add some link router

--- Data/frontend/src/Routes/ClientRoute.js
+++ Data/frontend/src/Routes/ClientRoute.js
@@ -5,11 +5,21 @@ import Home from "../pages/Home/Home";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import PrivateRoutes from "./PrivateRoutes";
 import Users from "../Component/Users/Users";
+import Login_Admin from "../pages/Admin/Login_Admin";
+import Ticket from "../pages/Ticket/Ticket";
+import InfoCar from "../Component/Info Car/InfoCar";
+import Add_Ticket from "../pages/Ticket/Add_Ticket";
 const ClientRoute = () => {
 	return (
 		<div>
 			<Switch>
 				<PrivateRoutes path="/users" component={Users} />
+				<PrivateRoutes path="/ticket" exact component={Ticket} />
+				<PrivateRoutes path="/info-car/id=:id_user" component={InfoCar} />
+				<PrivateRoutes path="/ticket/create" component={Add_Ticket} />
+				<Route path="/admin">
+					<Login_Admin />
+				</Route>
 				<Route path="/login">
 					<Login />
 				</Route>
