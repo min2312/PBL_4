9e8e86ef4987081bece52113075eca56923a6b76
Nguyen Quang Minh <quangminh23122004@gmail.com>
2024-10-19 14:09:57 +0700

    Add Hanlde controller api

--- Data/backend/src/routes/web.js
+++ Data/backend/src/routes/web.js
@@ -13,12 +13,14 @@ let initWebRoutes = (app) => {
 	router.get("/api/get-all-user", userController.HandleGetAllUser);
 	router.get("/api/get-info-car", userController.HandleGetInfoCar);
 	router.get("/api/getAllCar", apiController.HandleGetAllCar);
+	router.get("/api/getAllCar_Ticket", apiController.HandleGetAllCar_Ticket);
 	router.get("/api/account", userController.getUserAccount);
 	router.get("/api/getTypeTicket", apiController.HandleGetTypeTicket);
 	router.put("/api/edit-user", userController.HandleEditUser);
 	router.post("/api/create-new-user", userController.HandleCreateNewUser);
 	router.post("/api/create-new-car", apiController.HandleCreateNewCar);
 	router.post("/api/create-ticket", apiController.HandleCreatePayment);
+	router.post("/api/delete-ticket", apiController.HandleDeleteTicket);
 	router.delete("/api/delete-user", userController.HandleDeleteUser);
 	return app.use("/", router);
 };
