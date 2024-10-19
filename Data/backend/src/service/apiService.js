fd360bde30a27198b16d24a4918f17ee3ad3cfbf
Nguyen Quang Minh <quangminh23122004@gmail.com>
2024-10-19 11:29:28 +0700

    Add (Delete Ticket) and Get User Ticket

--- Data/backend/src/service/apiService.js
+++ Data/backend/src/service/apiService.js
@@ -58,10 +58,110 @@ let GetAllCar = (Idcar) => {
 						"updatedAt",
 					],
 					where: { id_user: iduser[i] },
+					include: [
+						{
+							model: db.Reservation,
+							attributes: ["type", "price"],
+						},
+					],
 				});
-				allCar = allCar.concat(data);
+				let data_noReservation = await db.Car.findAll({
+					where: { id_user: iduser[i], id_reservation: null },
+				});
+				for (let car of data) {
+					let payment = await db.Payment.findAll({
+						where: { id_car: car.id_car },
+						attributes: ["paymentDate"],
+					});
+					if (payment.length > 0) {
+						allCar.push({
+							...car.toJSON(),
+							paymentDate: payment[0].paymentDate,
+						});
+					}
+				}
+				allCar = allCar.concat(data_noReservation);
 			}
 			resolve(allCar);
+		} catch (e) {
+			reject(e);
+		}
+	});
+};
+let GetCar_Ticket = (id_user) => {
+	return new Promise(async (resolve, reject) => {
+		try {
+			let iduser = Array.isArray(id_user) ? id_user : id_user.split(",");
+			let allCar = [];
+			for (let i = 0; i < iduser.length; i++) {
+				let data = await db.Car.findAll({
+					attributes: [
+						"id_car",
+						"name",
+						"license_plate",
+						"type",
+						"inTime",
+						"outTime",
+						"id_user",
+						"id_reservation",
+						"createdAt",
+						"updatedAt",
+					],
+					where: {
+						id_user: iduser[i],
+						id_reservation: { [db.Sequelize.Op.ne]: null },
+					},
+					include: [
+						{
+							model: db.Reservation,
+							attributes: ["type", "price"],
+						},
+					],
+				});
+				for (let car of data) {
+					let payment = await db.Payment.findAll({
+						where: { id_car: car.id_car },
+						attributes: ["paymentDate"],
+					});
+					if (payment.length > 0) {
+						allCar.push({
+							...car.toJSON(),
+							paymentDate: payment[0].paymentDate,
+						});
+					}
+				}
+			}
+			resolve(allCar);
+		} catch (e) {
+			reject(e);
+		}
+	});
+};
+let DeleteTicket = (id_car) => {
+	return new Promise(async (resolve, reject) => {
+		try {
+			console.log("id", id_car);
+			let payment = await db.Payment.findOne({
+				where: { id_car: id_car },
+			});
+			if (payment) {
+				await db.Payment.destroy({
+					where: { id_car: id_car },
+				});
+				await db.Car.update(
+					{ id_reservation: null },
+					{ where: { id_car: id_car } }
+				);
+				resolve({
+					errCode: 0,
+					message: `The Ticket is deleted`,
+				});
+			} else {
+				resolve({
+					errCode: 2,
+					errMessage: `The Ticket isn't exist`,
+				});
+			}
 		} catch (e) {
 			reject(e);
 		}
@@ -112,33 +212,43 @@ let CreatePayment = (data) => {
 	return new Promise(async (resolve, reject) => {
 		try {
 			let date = formatDateForDatabase(data.paymentDate);
+			let currentDate = new Date();
 			let car_account = await db.Car.findOne({
 				where: { license_plate: data.licenseplate },
 			});
 			let reservation = await db.Reservation.findOne({
 				where: { type: data.type },
 			});
-
 			if (!car_account) {
 				return resolve({
 					errCode: 1,
 					errMessage: "Car not found.",
 				});
 			}
-
 			let payment = await db.Payment.findOne({
-				where: { id_car: car_account.id_car, amount: data.price },
+				where: { id_car: car_account.id_car },
 			});
-
 			if (payment) {
-				await db.Payment.update(
-					{ amount: data.price, paymentDate: date, id_car: car_account.id_car },
-					{ where: { id_payment: payment.id_payment } }
-				);
-				resolve({
-					errCode: 0,
-					errMessage: "Ticket updated successfully.",
-				});
+				let paymentDate = new Date(payment.paymentDate);
+				if (paymentDate >= currentDate) {
+					return resolve({
+						errCode: 2,
+						errMessage: "Payment is still valid, cannot create a new ticket.",
+					});
+				} else {
+					await db.Payment.update(
+						{
+							amount: data.price,
+							paymentDate: date,
+							id_car: car_account.id_car,
+						},
+						{ where: { id_payment: payment.id_payment } }
+					);
+					resolve({
+						errCode: 0,
+						errMessage: "Ticket updated successfully.",
+					});
+				}
 			} else {
 				if (reservation) {
 					await db.Payment.create({
@@ -166,7 +276,6 @@ let CreatePayment = (data) => {
 		}
 	});
 };
-
 let CheckLicensePlate = (LicensePlate) => {
 	return new Promise(async (resolve, reject) => {
 		try {
@@ -199,6 +308,8 @@ module.exports = {
 	CreateNewCar,
 	GetAllCar,
 	DeleteCar,
+	DeleteTicket,
 	GetTicketType,
 	CreatePayment,
+	GetCar_Ticket,
 };
