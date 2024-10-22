69bc26e5764033b094838c0ce7010f1d6f294f07
Nguyen Quang Minh <quangminh23122004@gmail.com>
2024-10-22 15:57:21 +0700

    Add Service Ticket

--- Data/frontend/src/services/apiService.js
+++ Data/frontend/src/services/apiService.js
@@ -15,10 +15,32 @@ const getAllCar = async (InputId) => {
 			console.log(err);
 		});
 };
+const getAllCar_Ticket = async (InputId) => {
+	let idQuery = Array.isArray(InputId) ? InputId.join(",") : InputId;
+	return axios
+		.get(`/api/getAllCar_Ticket?id=${idQuery}`)
+		.then((response) => {
+			return response;
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+};
+
 const getTypeTicket = async (type_ticket) => {
 	return axios.get(`/api/getTypeTicket?type=${type_ticket}`);
 };
 const CreateTicket = async (data) => {
 	return axios.post("/api/create-ticket", data);
 };
-export { CreateNewCar, getAllCar, getTypeTicket, CreateTicket };
+const DeleteTicket = async (data) => {
+	return axios.post("/api/delete-ticket", data);
+};
+export {
+	CreateNewCar,
+	getAllCar,
+	getAllCar_Ticket,
+	getTypeTicket,
+	CreateTicket,
+	DeleteTicket,
+};
